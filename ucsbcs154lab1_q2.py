fb494da88f8bf008069c742996824651d08554b4
Advait Arumugam <advaitarumugam@ucsb.edu>
2023-01-17 19:48:58 -0800

    initial

--- ucsbcs154lab1_q2.py
+++ ucsbcs154lab1_q2.py
@@ -8,7 +8,7 @@ import pyrtl
 
 val_a = pyrtl.Input(bitwidth=1, name='a')
 val_b = pyrtl.Input(bitwidth=1, name='b')
-select = pyrtl.Input(bitwidth=1, name='select')
+s = pyrtl.Input(bitwidth=1, name='select')
 result = pyrtl.Output(bitwidth=1, name='result')
 
 # Important: Assignments inside a "conditional_assignment"
@@ -20,7 +20,7 @@ result = pyrtl.Output(bitwidth=1, name='result')
 #    result |= val_b
 
 
-result <<= (val_a & select) | (val_b & select) 
+result <<= (val_a & s) | (val_b & s) 
 
 sim = pyrtl.Simulation()
 sim.step_multiple({'a': [0,1,0,1], 'b': [0,0,1,1], 'select': [0,1,0,1]})
